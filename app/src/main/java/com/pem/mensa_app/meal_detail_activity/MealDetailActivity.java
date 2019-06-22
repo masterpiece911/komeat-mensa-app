49d8f972665b90856e6c7bdd0d66f59d537ba215
Diana Irmscher <d.irmscher@campus.lmu.de>
2019-06-22 16:00:09 +0200

    Frist connecting to firebase

--- app/src/main/java/com/pem/mensa_app/meal_detail_activity/MealDetailActivity.java
+++ app/src/main/java/com/pem/mensa_app/meal_detail_activity/MealDetailActivity.java
@@ -1,12 +1,20 @@
 package com.pem.mensa_app.meal_detail_activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.pem.mensa_app.R;
 import com.pem.mensa_app.dummy.DummyContent;
 
@@ -21,6 +29,7 @@ public class MealDetailActivity extends AppCompatActivity implements CommentFrag
     private CommentRecyclerViewAdapter commentRecyclerViewAdapter;
     private RecyclerView.LayoutManager layoutManager;
 
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +45,26 @@ public class MealDetailActivity extends AppCompatActivity implements CommentFrag
 
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
+
+        DocumentReference docRef = db.collection("Meal").document("p7SSMzqa4qD61BY1kroM");
+
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot documentSnapshot = task.getResult();
+                    if (documentSnapshot.exists()) {
+                        Toast.makeText(MealDetailActivity.this, documentSnapshot.getId(), Toast.LENGTH_SHORT).show();
+                        Log.d("mealDetailActivity", documentSnapshot.getId());
+                    } else {
+                        Toast.makeText(MealDetailActivity.this, "Document is unknown.", Toast.LENGTH_SHORT).show();
+                        Log.d("mealDetailActivity", "Document is unknown");
+                    }
+                } else {
+                    Log.d("mealDetailActivity", "get failed with: ", task.getException());
+                }
+            }
+        });
 
         DummyContent.DummyItem dummy = new DummyContent.DummyItem( "id", "content", "details");
 
